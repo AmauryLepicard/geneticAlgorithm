91f54ff3c078b4388dc6c69647151b79d80c822c
lepicard.amaury@gmail.com <y-gtg+e8>
2018-03-28 19:18:02 +0200

    NN: Fixed layers sizes NN: added __str__ function

--- AIPlayer.py
+++ AIPlayer.py
@@ -1,10 +1,14 @@
 from NeuralNetwork import NeuralNetwork
 
+def relu(x):
+    return max(0.0, x)
+
 class AIPlayer:
     def __init__(self):
-        self.nn = NeuralNetwork(12, (6, 6), 3)
+        self.nn = NeuralNetwork(12, (6, 6), 3, relu)
 
     def DNA(self):
         return self.nn.DNA()
 
     def getData(self):
+        pass
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,31 +1,43 @@
 import numpy as np
-
+import pandas as pd
 
 class NeuralNetwork:
     def __init__(self, nbInputs, hiddenLayersSizes, nbOutputs, activationFunction):
         self.nbInputs = nbInputs
         self.nbOutputs = nbOutputs
+        self.activationFunction = np.vectorize(activationFunction)
+
         #input layer
-        self.layers = [NNLayer(nbInputs, nbInputs, activationFunction)]
+        self.layers = [NNLayer(nbInputs, nbInputs, self.activationFunction)]
 
         #hidden layers
-        for layerNb, neuronsNb in enumerate(hiddenLayersSizes):
-            hiddenLayer = NNLayer(self.layers[layerNb].neuronsNb, neuronsNb, activationFunction)
+        for neuronsNb in hiddenLayersSizes:
+            hiddenLayer = NNLayer(self.layers[-1].neuronsNb, neuronsNb, self.activationFunction)
             self.layers.append(hiddenLayer)
 
-        #output layer
-        self.layers.append(NNLayer(self.layers[-1].neuronsNb, nbOutputs, activationFunction))
+        # output layer
+        self.layers.append(NNLayer(self.layers[-1].neuronsNb, nbOutputs, self.activationFunction))
 
     def compute(self, inputVector):
-        if inputVector.shape != (1, self.nbInputs):
+        if len(inputVector) != self.nbInputs:
             raise ValueError("Incorrect inputVector size")
         result = inputVector
-        for layer in self.layers:
+        #print("Input :", result)
+        for i,layer in enumerate(self.layers):
             result = layer.compute(result)
+            #print("After layer", i, ":", result)
         return result
 
     def DNA(self):
         return np.concatenate([l.weights.flatten() for l in self.layers])
+
+    def __str__(self):
+        s = ""
+        for il, layer in enumerate(self.layers):
+            s += "Layer " + str(il) + " (" + str(layer.inputNb) + "," + str(layer.neuronsNb) + "):\n"
+            df = pd.DataFrame(layer.weights)
+            s += df.to_string(header=False, index=False) + "\n\n"
+        return s
 
 class NNLayer:
     def __init__(self, inputNb, neuronsNb, activationFunction):
@@ -35,9 +47,10 @@ class NNLayer:
         self.weights = np.random.rand(inputNb, neuronsNb)
 
     def compute(self, inputVector):
-        if inputVector.shape != (1, self.inputNb):
+        if len(inputVector) != self.inputNb:
             raise ValueError("Incorrect inputVector size")
         b = np.matmul(inputVector, self.weights)
 
         outputVector = self.activationFunction(b)
         return outputVector
+
